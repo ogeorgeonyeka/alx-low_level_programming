c7da8e94f986eab367d31d8ef6016f89eec8a684
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-17 12:00:02 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A progam that prints a line to the standard error
+ * Return: 1 (success)
+int main(void)
+{
+fprint("and that piece of art is useful\" - Dora Korper, 2015-10-19\n", stdout);
+return (1);
+}
