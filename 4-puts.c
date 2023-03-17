94eb68367e9c5f1bfcb84b9b319b6fde6f024bd4
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-17 16:11:08 -0700

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - The puts main function 
+ * Return: 0 always
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
