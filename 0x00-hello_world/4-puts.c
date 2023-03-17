896c1d88f84a70513c83523b065ce20c31c48ba4
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-17 11:38:55 -0700

    100-intel

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - The C program function
+ *
+ * Return: 0 always
+ */
+
+
+int main(void)
+		
+{	
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+}
