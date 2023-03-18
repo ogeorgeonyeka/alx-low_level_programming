ab16275bf8d35ea0fe584c5cf91549951d5c1fad
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-18 09:53:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 /**
- * main - A c program that prints a line
+ * main - main function
+ *
  * Return: 1 always
  */
 
 int main(void)
 {
-int fprintf("and that piece of art is useful\n" - Dora);Korper; 2015-10-19;
-return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
