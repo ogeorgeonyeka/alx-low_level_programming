5bf69d38e4d55eb0d3b437055a1813d119a7d4af
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-19 13:43:10 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,17 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 0 ; i < 10 ; i++)
-        {
-                putchar(i + '0');
-                if (i != 9)
-                {
-                        putchar(',');
-                        putchar(' ');
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
+	return (0);
 }
