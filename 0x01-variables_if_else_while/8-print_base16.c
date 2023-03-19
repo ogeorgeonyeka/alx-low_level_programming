0b4590318684a7c4a1d2efb9ea1bb23aba5239e4
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-19 13:26:01 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,16 +9,12 @@
 int main(void)
 {
 	int i;
+	char j;
 
 	for (i = 0 ; i < 10 ; i++)
-	{
 		putchar(i + '0');
-		if (i != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
