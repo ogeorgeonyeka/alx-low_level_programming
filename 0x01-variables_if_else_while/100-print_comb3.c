54f173f0ffb21bd5d2a9ef9574149eda24be69f1
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-20 00:12:37 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				if (i + j != 17)
-				}	
+				{
 					putchar(',');
 					putchar(' ');
 				}
@@ -30,4 +30,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
