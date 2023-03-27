e73c34cb29648524b57cbb442c21d99072257a2e
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-27 09:00:48 -0700

    half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints character of a string
+ * @str: function parameter
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
