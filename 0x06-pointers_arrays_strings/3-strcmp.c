6ba5156a9c78c4337e20c64878a695cf94fa02fc
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-31 02:43:17 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,21 @@
 
 /**
  * _strcmp - compares two strings values
- * @s1: string 1
- * @s2: string 2
- * Return: the difference in s1 and s2
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * Return: value < 0, value > 0 & 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int counter, compare_value;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] = s2[i]);
-		}
-	i++;
+		counter++;
 	}
-	return (0);
+
+	compare_value = s1[counter] - s2[counter];
+	return (compare_value);
 }
