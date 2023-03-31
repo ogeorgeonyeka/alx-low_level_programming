067b8f53285e5e9937cf92236880d13cdcfd051c
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-30 19:16:42 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,9 +15,9 @@ char *leet(char *n)
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (i = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[i])
+			if (n[i] == s1[j])
 			{
 				n[i] = s2[j];
 			}
