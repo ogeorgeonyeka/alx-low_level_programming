3ed539e8a16a4c8960a7e17bcf2f3766d5dce8ed
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-03-30 18:11:22 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,17 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] = s2[i]);
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
 		}
-	i++;
+		else
+			return (0);
 	}
 	return (0);
 }
