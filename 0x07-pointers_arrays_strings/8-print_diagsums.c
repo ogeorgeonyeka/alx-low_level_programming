8113a0452e0fc4f0936ceaca4d18e020e20d5904
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-04-04 00:03:27 -0700

    double fun

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of two diagonals of source matrix
+ * @a: the matrix of integers
+ * @size: the size of the matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int index, sum1 = 0, sum2 = 0;
+
+	for (index = 0; index < size; index++)
+	{
+		sum1 += a[index];
+		a += size;
+	}
+
+		a -= size;
+
+		for (index = 0; index < size; index++)
+		{
+			sum2 += a[index];
+			a -= size;
+		}
+		printf("%d", "%d", sum1, sum2);
+
+}
