8d74b2de03dab25c8166d9281a62c699c8352c5f
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-04-03 22:50:05 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to be searched
+ * @needle: the substring to be located
+ *
+ * Return: if the substring is located
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+	int accept = 0;
+
+	for(index = 0; accept[index]; index++)
+	{
+
+		if (*needle == 0)
+			return (haystack);
+
+		while (*haystack)
+		{
+			index = 0;
+
+			if (haystack[index] == needle[index])
+			{
+				do {
+					if (needle[index + 1] == '\0')
+						return (haystack);
+
+					index++;
+
+				} while (haystack[index] == needle[index]);
+
+			}
+
+			haystack++;
+		}
+
+		return ('\0');
+	
